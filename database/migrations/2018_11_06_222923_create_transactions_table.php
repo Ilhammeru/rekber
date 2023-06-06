e3a522f6578439f0d4fdd4f68bc10d10cd9c76a2
Ilham Gumilang <gumilang.dev@gmail.com>
2023-06-06 12:21:13 +0700

    fix migration bugs

--- database/migrations/2018_11_06_222923_create_transactions_table.php
+++ database/migrations/2018_11_06_222923_create_transactions_table.php
@@ -34,6 +34,6 @@ return new class() extends Migration {
 
     public function down(): void
     {
-        Schema::dropIfExists('table');('transactions');
+        Schema::dropIfExists('transactions');
     }
 };
